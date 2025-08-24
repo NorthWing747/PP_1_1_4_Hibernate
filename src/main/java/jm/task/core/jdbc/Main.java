2305ac514634976fec086e44b57dc5bd3690d6c5
Anton <antuhinanton@rambler.ru>
2025-08-24 13:01:25 +0300

    1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,36 @@
 package jm.task.core.jdbc;
 
+//import com.sun.jdi.connect.spi.Connection;
+import java.sql.Connection;
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
+
         // реализуйте алгоритм здесь
+        try (Connection conn = Util.getConnection()) {
+            if (conn != null) {
+                System.out.println("БД доступна, можно работать");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Антон", "Иванов", (byte) 25);
+        userService.saveUser("Мария", "Петрова", (byte) 30);
+        userService.saveUser("Степан", "Петров", (byte) 33);
+        userService.saveUser("Екатерина", "Иванова", (byte) 30);
+
+        System.out.println(userService.getAllUsers());
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+//import com.sun.jdi.connect.spi.Connection;
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.*;
+
 public class Util {
+
     // реализуйте настройку соеденения с БД
+    private static String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static String USER = "root";
+    private static String PASSWORD = "root";
+
+    // Метод для получения соединения
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            // Подгрузка драйвера
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Соединение установлено!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Ошибка подключения к БД!");
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
