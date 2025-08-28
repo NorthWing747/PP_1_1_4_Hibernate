924ec159c3cd01502c6580e5460ebf7e6f1dec39
Anton <antuhinanton@rambler.ru>
2025-08-28 15:18:10 +0300

    git commty - m

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,7 +6,7 @@ import jm.task.core.jdbc.model.User;
 
 public class Main {
     public static void main(String[] args) {
-        UserDao userDao = new UserDaoHibernateImpl();
+        //UserDao userDao = new UserDaoHibernateImpl();
 
         userDao.createUsersTable();
 
